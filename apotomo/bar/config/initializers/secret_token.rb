f34dd93b78951e43043049d15a8e12e2dd51489d
Tomas Valent <equivalent@eq8.eu>
2012-06-22 16:33:34 +0100

    starting investigatinog Apotomo gem

---
+++ apotomo/bar/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bar::Application.config.secret_token = 'bb931ae14126b7ac7295eab92f3ac6a9d0f30f88b1890fe16d29fef86f6bd5257f90903d5d22d36998bf62ae71216772de2db34a8e9305ad59934a761773ba68'
